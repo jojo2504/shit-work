764d99fb20224e182bb2d08d965054840e8920c7
jojo2504 <jojo.duchuy@gmail.com>
2023-10-26 20:17:29 +0200

    aaaaaaa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace NamespacePlaceholder{
+    class Program{
+        private static void Main(string[] args){
+            Console.WriteLine("aaaaaaa");
+        }
+    }
+}
