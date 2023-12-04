7cc468df32e340bd94af23659615b0b2a1b6e45b
jojo2504 <jojo.duchuy@gmail.com>
2023-12-04 18:48:56 +0100

    exo 3 tpo3 p2

--- Program.cs
+++ Program.cs
@@ -2,8 +2,43 @@
 
 namespace NamespacePlaceholder{
     class Program{
+
+        static bool ex3(string sentence){
+            const string MAJ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            bool checker = true;
+            if(!(MAJ.Contains(sentence[0]) && sentence[sentence.Length-1] == '.')){
+                checker = false;
+            }
+
+            for (int i = 0; i < sentence.Length-2 && checker; i++){
+
+                if (sentence[i] == ' ' && sentence[i+1] == ' '){
+                    checker = false;
+                }
+
+                if (sentence[i] == '.' && sentence[i+1] != ' '){
+                    checker = false;
+                }
+
+                if (sentence[i] == ',' && sentence[i+1] != ' '){
+                    checker = false;
+                }
+
+                if (i <= sentence.Length-3){
+                    if (sentence[i] != ' ' && sentence[i+1] == ';' && sentence[i+2] == ' '){
+                        checker = false;
+                    }
+
+                    if (sentence[i] != ' ' && sentence[i+1] == ':' && sentence[i+2] == ' '){
+                        checker = false;
+                    }
+                }
+            }
+
+            return checker;
+        }
         private static void Main(string[] args){
-            Console.WriteLine("aaaaaaa");
+            Console.WriteLine(ex3("Bonjour j'aime les haricots."));
         }
     }
 }
